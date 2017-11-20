c5e7eed03850406ef098bcefb4458b41f2c82225
Jeff Nabors <bulletr0k@gmail.com>
2017-11-20 09:09:29 -0500

    Added file structure, HTML wireframe and basic css and JS outline.

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+//Make the button begin the function that runs the quiz.
+
+//When the game begins, javascript should clear the DOM's title page and add the first question to the existing div's.
+
+//After the question is displayed, a timer should appear that counts down from 30 to 0
+
+//If the player has not clicked an answer before the timer reaches 0 or selects the wrong answer, add one to wrong answers
+
+//If the player selects the correct answer, add one to right answers.
+
+//Once the player selects an answer, the next question should load.
+
+//After all questions have been answered, the DOM should change to a final screen that shows how many correct answers the player had.
